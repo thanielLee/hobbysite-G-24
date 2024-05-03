d68c9452d9c2ffc91339c086c32940a96c6a69ba
thanieLee <thancraft123gaming@gmail.com>
2024-05-03 10:38:29 +0800

    user_management: added profile updates

--- user_management/models.py
+++ user_management/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.urls import reverse
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="Profile")
@@ -9,6 +10,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.display_name
+
+    def get_absolute_url(self):
+        return reverse('profile:update', args=[str(self.pk)])
     
     
 def create_user_profile(sender, instance, created, **kwargs):
