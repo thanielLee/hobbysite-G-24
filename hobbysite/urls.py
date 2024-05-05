f2b6f77b63dc140044bd9e479d7918d915affd60
EnnaColada <enna.veloso@student.ateneo.edu>
2024-05-06 02:30:57 +0800

    modified settings and urls so images would render

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import index
+from django.conf import settings                
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +31,5 @@ urlpatterns = [
     path('', index, name="home"),
     path('profile/', include('user_management.urls', namespace='profiles'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
