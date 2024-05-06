27fa3a36580f1b4bac6c49612f0177c9c595eb7e
JavaFoxl2 <justine.capili@student.ateneo.edu>
2024-05-07 00:24:19 +0800

    merchstore: models added

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,33 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
-
+from user_management.models import Profile
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
-    def __str__ (self):
-        return self.name
-    
     class Meta:
-        ordering = ['name', ]
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    producttype = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    producttype = models.ForeignKey (
-        ProductType, 
-        null = True, 
-        on_delete=models.SET_NULL, 
-        related_name = 'products'
-    )
+    stock = models.IntegerField()
+    STATUS_CHOICES = [
+        ('available', 'Available'),
+        ('on_sale', 'On Sale'),
+        ('out_of_stock', 'Out of Stock'),
+    ]
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='available')
 
-    def _str__(self):
+    def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return reverse('merchstore:product_detail', args=[self.pk])
+    def save(self, *args, **kwargs):
+        if self.stock == 0:
+            self.status = 'out_of_stock'
+        super().save(*args, **kwargs)
 
-    class Meta:
-        ordering = ['name', ]
+class Transaction(models.Model):
+    buyer = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    amount = models.IntegerField()
+    STATUS_CHOICES = [
+        ('on_cart', 'On Cart'),
+        ('to_pay', 'To Pay'),
+        ('to_ship', 'To Ship'),
+        ('to_receive', 'To Receive'),
+        ('delivered', 'Delivered'),
+    ]
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    created_on = models.DateTimeField(auto_now_add=True)
