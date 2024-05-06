b9501bd0673f02cd7f8b02f5d3b484c50073750c
JavaFoxl2 <justine.capili@student.ateneo.edu>
2024-05-07 00:26:03 +0800

    merchstore: urls added

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-
-from .views import ProductListView, ProductDetailView
-
+from .views import (
+    ProductListView, ProductDetailView, ProductCreateView,
+    ProductUpdateView, CartView, TransactionsListView
+)
 
 urlpatterns = [
-    path('items', ProductListView.as_view(), name='product_list'),
-    path('item/<int:pk>', ProductDetailView.as_view(), name='product_detail'), 
+    path('items/', ProductListView.as_view(), name='product_list'),
+    path('item/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('item/add/', ProductCreateView.as_view(), name='product_create'),
+    path('item/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_update'),
+    path('cart/', CartView.as_view(), name='cart_view'),
+    path('transactions/', TransactionsListView.as_view(), name='transactions_list'),
 ]
 
-app_name = "merchstore"
+app_name = 'merchstore'
