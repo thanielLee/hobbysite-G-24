52feae02966ccb1148ef294b8f7a7756436e9e85
JavaFoxl2 <justine.capili@student.ateneo.edu>
2024-05-08 15:02:14 +0800

    merchstore: updated admin.py

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,22 +1,24 @@
 from django.contrib import admin
-from .models import ProductType, Product, Transaction, Profile
 
-@admin.register(ProductType)
+from .models import Product, ProductType, Transaction
+
+
 class ProductTypeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description']
+    model = ProductType
 
-@admin.register(Product)
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'producttype', 'owner', 'price', 'stock', 'status']
-    list_filter = ['producttype', 'status']
-    search_fields = ['name']
+    model = Product
 
-@admin.register(Transaction)
+    list_display = ["name", "price", "type"]
+    list_filter = ["type"]
+    search_fields = ["name"]
+
+
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ['buyer', 'product', 'amount', 'status', 'created_on']
-    list_filter = ['status', 'created_on']
-    search_fields = ['product__name', 'buyer__user__username']
+    model = Transaction
 
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user']
+
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Transaction, TransactionAdmin)
