a5e366f8665ef3f1acd40b1dc5a2cdbc9b199db3
thanielLee <thaniel.lee@student.ateneo.edu>
2024-05-08 15:37:05 +0000

    main: upadted for digital ocean

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['188.166.250.66']
 
 
 # Application definition
